8500146b5ae1815c80583c9253e0f36ca4955dbc
Pomonoli <171513114+Pomonoli@users.noreply.github.com>
2026-08-31 20:48:47 +0200

    Update server version to 1.11.8

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ runtime.startMaintenance();
 startDatabaseBackups();
 
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Pluto v1.11.7 draait op poort ${PORT}`);
+  console.log(`Pluto v1.11.8 draait op poort ${PORT}`);
   console.log(`SQLite: ${authDb.DB_PATH}`);
 });
